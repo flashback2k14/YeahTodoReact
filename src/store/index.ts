e2dba4500b15ada56b3d681ceb59e09104511efc
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-08-01 10:02:30 +0200

    remove dependencies

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
-import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { IState, reducer } from "../reducers/index";
 
 const store = createStore<IState, any, any, any>(
   reducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger)
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 export default store;
