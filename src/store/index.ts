bcd1b329accceb106bc4bc72d930e914d65bc289
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-09-08 21:32:53 +0200

    fix deployment

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { IState, reducer } from "../reducers/index";
 
 const store = createStore<IState, any, any, any>(
   reducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  (<any>window).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (<any>window).__REDUX_DEVTOOLS_EXTENSION__()
 );
 export default store;
