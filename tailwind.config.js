b605e31267c0387d225c551eb7d6250f7ee19775
Sebastian Kloppe <sebastian.kloppe@smartcommerce.de>
2024-09-24 14:15:51 +0200

    fix custom font error #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: 'Lato, sans-serif',
+      sans: '"Lato", system-ui',
     },
     extend: {
       boxShadow: {
